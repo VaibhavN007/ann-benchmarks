709ccb5550580f75979ea3d1a2b3bd960f63597f
VaibhavN007 <vaibhavnit007@gmail.com>
2018-07-05 15:08:02 +0530

    Update upload image script

--- upload-images.sh
+++ upload-images.sh
@@ -4,6 +4,10 @@ git config --global user.email 'vaibhavnit007@gmail.com'
 git remote rm origin
 git remote add origin https://VaibhavN007:${GITHUB_API_KEY}@github.com/VaibhavN007/ann-benchmarks.git
 
-git add *
-git commit -m "First push from Travis CI"
+git status
+git add .
+git status
+git commit -m "Second push from Travis CI"
+git status
 git push -f origin deploy_glove
+git status
