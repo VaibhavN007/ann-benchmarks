2fea268ac6554184f26b809b48ac904bd8966522
Vaibhav Nitnaware/MGMT Support/Contractor/Samsung Electronics <vaibhav.21@corp.samsungelectronics.net>
2018-07-05 13:21:36 +0530

    First push using API key

--- upload-images.sh
+++ upload-images.sh
@@ -2,5 +2,5 @@ git remote rm origin
 git remote add origin https://VaibhavN007:${GITHUB_API_KEY}@github.com/VaibhavN007/ann-benchmarks.git
 
 git add *
-git commit -m "First push from Travis CI"
+git commit -m "First push using API key"
 git push origin deploy_glove
