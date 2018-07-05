e2bccad225a5cb7f35fc593816be8db497892d05
VaibhavN007 <vaibhavnit007@gmail.com>
2018-07-05 14:52:46 +0530

    Change commit message and delete results images

--- upload-images.sh
+++ upload-images.sh
@@ -5,5 +5,5 @@ git remote rm origin
 git remote add origin https://VaibhavN007:${GITHUB_API_KEY}@github.com/VaibhavN007/ann-benchmarks.git
 
 git add *
-git commit -m "First push using API key"
+git commit -m "First push from Travis CI"
 git push -f origin deploy_glove
