b5ce49eff0c664b1f8d39e1a3b540e9c6abb74c5
VaibhavN007 <vaibhavnit007@gmail.com>
2018-07-05 13:34:42 +0530

    First push using API key

--- upload-images.sh
+++ upload-images.sh
@@ -1,5 +1,5 @@
-git config --global user.name 'VaibhavN007'
-git config --global user.email 'vaibhavnit007@gmail.com'
+# git config --global user.name 'VaibhavN007'
+# git config --global user.email 'vaibhavnit007@gmail.com'
 
 git remote rm origin
 git remote add origin https://VaibhavN007:${GITHUB_API_KEY}@github.com/VaibhavN007/ann-benchmarks.git
