be700089e102f7e50ddf9a800e8408a8f1a3d5af
VaibhavN007 <vaibhavnit007@gmail.com>
2018-07-05 16:16:59 +0530

    Push to ann-results repo

--- upload-images.sh
+++ upload-images.sh
@@ -7,7 +7,22 @@ git remote add origin https://VaibhavN007:${GITHUB_API_KEY}@github.com/VaibhavN0
 git status
 git add .
 git status
-git commit -m "Second push from Travis CI"
+git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 git status
 git push -f origin deploy_glove
+git status
+
+cd $HOME
+git clone https://VaibhavN007:{GITHUB_API_KEY}@github.com/VaibhavN007/ann-results
+ls -R
+cd ann-results/
+cp ../ann-benchmarks/*.html
+cp ../ann-benchmarks/*.png
+mkdir -p results
+cp -r ../ann-benchmarks/results/* ./results/
+git status
+git add .
+git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+git status
+git push origin master
 git status
